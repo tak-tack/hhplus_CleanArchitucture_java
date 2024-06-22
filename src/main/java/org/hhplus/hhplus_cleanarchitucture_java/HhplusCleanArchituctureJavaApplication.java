ef364004c89a0706d049e98903c689428b5b4be7
tak-tack <kth302303@gmail.com>
2024-06-22 21:32:23 +0900

    2024-06-22 프로젝트 시작

---
+++ src/main/java/org/hhplus/hhplus_cleanarchitucture_java/HhplusCleanArchituctureJavaApplication.java
@@ -0,0 +1,13 @@
+package org.hhplus.hhplus_cleanarchitucture_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusCleanArchituctureJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusCleanArchituctureJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hhplus/hhplus_cleanarchitucture_java/HhplusCleanArchituctureJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hhplus.hhplus_cleanarchitucture_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusCleanArchituctureJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
